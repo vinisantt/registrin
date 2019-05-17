b18823bd5af719a3dbc4b9ced5815801dc183d64
CarllosD <carlosd@rede.ulbra.br>
2019-05-17 17:16:45 -0300

    registrinho de ponto melhorado e atributo descricao add em confHorario

--- frequencia/admin.py
+++ frequencia/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import *
+from .models import ConfHorario, Funcionario, Justificativa, Registro_Ponto
 
 # Register your models here.
 
 admin.site.register(Funcionario)
 admin.site.register(Justificativa)
 admin.site.register(ConfHorario)
-admin.site.register(Registro_Ponto)
+
+@admin.register(Registro_Ponto)
+class Registro_PontoAdmin(admin.ModelAdmin):
+        readonly_fields = ["ip", "entrada"]
--- frequencia/models.py
+++ frequencia/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,14 +10,14 @@ class ConfHorario(models.Model):
     hSaida = models.TimeField("Configuração de Horario (Saída)", auto_now=False, auto_now_add=False)
 
     def __str__(self):
-        return "Horário de entrada: {}  Horário de saída: {}".format(str(self.hEntrada), str(self.hSaida))
+        return "{} | Horário de entrada: {}  Horário de saída: {}".format(self.descricao, str(self.hEntrada), str(self.hSaida))
 
 class Funcionario(models.Model):
-        usuario = models.CharField(verbose_name="Nome de usuário blog",max_length = 128)
+        usuario = models.CharField(verbose_name="Nome de usuário",max_length = 128)
         nome = models.CharField(verbose_name="Nome Completo", max_length = 128)
         data_de_nascimento = models.CharField(
             'Data de nascimento', blank=True, null=True,max_length = 128)
-        chefe = models.ForeignKey("Funcionario",blank=True, null=True,verbose_name="AAA", on_delete=models.CASCADE)
+        chefe = models.ForeignKey("Funcionario",blank=True, null=True,verbose_name="Superior", on_delete=models.CASCADE)
         confHora = models.ManyToManyField(ConfHorario, verbose_name="Horarios")
         
         def __str__(self):
@@ -30,22 +31,35 @@ class Justificativa(models.Model):
     funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
 
 class Registro_Ponto(models.Model):
-    entrada= models.DateTimeField(verbose_name= "Horário de Entrada", 
+    class Meta:
+        verbose_name = 'Registro de Ponto'
+        verbose_name_plural= 'Registros de Ponto'
+
+    entrada= models.DateTimeField(verbose_name= "Entrada", 
         auto_now_add=True,
         )
-    saida= models.DateTimeField(verbose_name= "", 
+    saida= models.DateTimeField(verbose_name= "Saída", 
         auto_now_add=True,
         )
-    ip= models.CharField("Ip da Funcionario", 
-        max_length=50, 
+    ip= models.CharField(verbose_name="Ip do Funcionario", 
+        max_length=15, 
         blank=True, 
-        null=True
+        null=True,
         )
-    #status= models.ForeignKey(Status, on_delete=models.CASCADE,verbose_name= "Status do Funcionário",)
-    funcionario= models.OneToOneField(
-        Funcionario,
+    '''
+    status= models.ForeignKey(Status, 
+        on_delete=models.CASCADE,
+        verbose_name= "Status do Funcionário",
+        blank=True, 
+        null=True,
+        )
+    '''
+    funcionario= models.OneToOneField(Funcionario, 
         on_delete=models.CASCADE,
         primary_key=True,
         )
 
-    
+    def __str__(self):
+        return 'Registro'.format(self.funcionario.nome)
+
+
