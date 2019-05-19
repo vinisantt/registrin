83e4006930bb29a10be7d6f058730d8d6e48e310
vinicius76 <vdias6849@gmail.com>
2019-05-19 13:15:30 -0300

    tava faltando umas coisas

--- frequencia/views.py
+++ frequencia/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.views.generic import ListView, FormView,TemplateView
+from django.http import *
+
+
+def bateponto(request):
+    return render(request,'frequencia/index.html'
+)
