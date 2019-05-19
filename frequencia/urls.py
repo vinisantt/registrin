bb21d5730541a8ca504e9560a32eb04f952bb751
ThigSchuch <thigschuch@gmail.com>
2019-05-19 12:39:04 -0300

    Tela inicial é o LOGIN

--- frequencia/urls.py
+++ frequencia/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name = 'login'),
+    path('', auth_views.LoginView.as_view(), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
