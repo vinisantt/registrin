3a2ea94fe64e9bebffd92df19acee73accbead17
vinicius76 <vdias6849@gmail.com>
2019-05-19 13:18:45 -0300

    agora sim ta certo kkkkk tava faltando as views e url

--- frequencia/urls.py
+++ frequencia/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from . import views
+from .views import *
 
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('registro_batida/',bateponto,name='ponto')
 ]
