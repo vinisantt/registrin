48048eb68147a5b8b337fe23b3531f3369ab3534
Caique Guedes <caique-guedes@hotmail.com>
2019-05-19 13:38:30 -0300

    agora foi men

--- frequencia/urls.py
+++ frequencia/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from django.conf.urls import url
+from django.conf import settings
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 from . import views
-
+from frequencia.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('registro_batida/', TemplateView.as_view(template_name='frequencia/timer.html'), name='logout'),
 ]
--- frequencia/views.py
+++ frequencia/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import messages
+from django.views.generic import ListView, FormView,TemplateView
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+
+class CadastroSucessoView(TemplateView):
+    template_name = 'registration/login.html'
